fa2dddc4d887285091c0e39a932b0032decd75bf
imjorgemor <imjorgemor@outlook.es>
2025-05-27 00:09:12 +0200

    add extra user slot

--- bookingP.js
+++ bookingP.js
@@ -14,7 +14,7 @@ const updateCronSchedule = async () => {
         const data = await response.text();
         const row = data.split('\n')[3].split('\r')[0].split(',');
         const row2 = data.split('\n')[7].split('\r')[0].split(',');
-        const row3 = data.split('\n')[9].split('\r')[0].split(',');
+        const row3 = data.split('\n')[10].split('\r')[0].split(',');
         const [, day, hour] = row
         const [, day1, hour1] = row2;
         const [, day2, hour2] = row3;
